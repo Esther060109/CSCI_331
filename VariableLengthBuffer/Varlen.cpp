ab63574e0a27a31cf8f3fcd0bc847e9404e06370
asble19 <ashleybui01@gmail.com>
2025-09-19 03:32:58 -0500

    Add VariableLengthBuffer .cpp and .h

--- VariableLengthBuffer/Varlen.cpp
+++ VariableLengthBuffer/Varlen.cpp
@@ -1,1 +1,81 @@
+#include <iostream>
+#include <cstring>
+#include "Varlen.h"
 
+VariableLengthBuffer::VariableLengthBuffer(int maxBytes)
+    : IOBuffer(maxBytes)
+{
+    Init();
+}
+
+void VariableLengthBuffer::Clear()
+{
+    IOBuffer::Clear();
+}
+
+int VariableLengthBuffer::Read(istream &stream)
+{
+    if (stream.eof()) return -1;
+    int recaddr = stream.tellg();
+    Clear();
+    unsigned short bufferSize;
+    stream.read((char*)&bufferSize, sizeof(bufferSize));
+    if (!stream.good())
+    {
+        stream.clear();
+        return -1;
+    }
+    stream.read(Buffer, bufferSize);
+    if (!stream.good())
+    {
+        stream.clear();
+        return -1;
+    }
+    return recaddr;
+}
+
+int VariableLengthBuffer::Write(ostream &stream) const
+{
+    int recaddr = stream.tellp();
+    unsigned short bufferSize = GetUsed(); // use actual size of data
+    stream.write((char*)&bufferSize, sizeof(bufferSize));
+    if (!stream.good()) return -1;
+    stream.write(Buffer, bufferSize);
+    if (!stream.good()) return -1;
+    return recaddr;
+}
+
+const char* headerStr = "Variable";
+const int headerSize = strlen(headerStr);
+
+int VariableLengthBuffer::ReadHeader(istream &stream)
+{
+    char str[headerSize + 1];
+    int result = IOBuffer::ReadHeader(stream);
+    if (!result) return 0;
+    stream.read(str, headerSize);
+    if (!stream.good()) return 0;
+    str[headerSize] = '\0';
+    if (strncmp(str, headerStr, headerSize) != 0) return 0;
+    return stream.tellg();
+}
+
+int VariableLengthBuffer::WriteHeader(ostream &stream) const
+{
+    int result = IOBuffer::WriteHeader(stream);
+    if (!result) return 0;
+    stream.write(headerStr, headerSize);
+    if (!stream.good()) return 0;
+    return stream.tellp();
+}
+
+void VariableLengthBuffer::Print(ostream &stream) const
+{
+    IOBuffer::Print(stream);
+}
+
+int VariableLengthBuffer::Init()
+{
+    Clear();
+    return 1; 
+}
--- VariableLengthBuffer/Varlen.h
+++ VariableLengthBuffer/Varlen.h
@@ -1,1 +1,37 @@
+#ifndef VARLEN_H
+#define VARLEN_H
 
+#include <iostream>
+#include <cstring>   
+#include "IOBuffer.h"
+
+using namespace std;
+
+class VariableLengthBuffer : public IOBuffer
+// Abstract class designed to support variable-length records
+// Fields may be of a variety of types
+{
+public:
+    VariableLengthBuffer(int MaxBytes = 1000);
+    
+    VariableLengthBuffer(const VariableLengthBuffer &buffer)
+        // copy constructor
+        : IOBuffer(buffer) {}
+
+    void Clear();                  // clear fields from buffer
+    int Read(istream &);           // read a buffer from stream
+    int Write(ostream &) const;    // write a buffer to stream
+    int ReadHeader(istream &);     // read the header from stream
+    int WriteHeader(ostream &) const; // write header to stream
+
+    // packing methods for CSV fields
+    int PackFixLen(void *, int);
+    int PackDelimeted(void *, int);
+    int PackLength(void *, int);
+
+    void Print(ostream &) const;
+    int SizeOfBuffer() const;      // return current size of buffer
+    int Init();
+};
+
+#endif
